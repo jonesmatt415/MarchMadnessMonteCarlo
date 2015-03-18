3a7ea8734ccd66470219e067b14c3040b94d5ab5
Michael g. Lerner <mglerner@gmail.com>
2015-03-18 14:15:50 -0400

    tentative HW added

---
+++ Homework/index.php
@@ -0,0 +1,7 @@
+<?php include("../header.php"); ?>
+<div id="container">
+<h1>Michael G. Lerner</h1> 
+<h2>Physics 375: Statistical and Thermal Physics</h2>
+<?php include("index.html"); ?>
+</div>
+</body>
